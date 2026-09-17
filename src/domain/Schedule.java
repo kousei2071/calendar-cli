ebd4862dcc5d37fdafa3d57b2282072ecc772976
kousei2071 <kousei2071@gmail.com>
2026-09-17 21:31:12 +0900

    fix: Itemsをitmesに変更

--- src/domain/Schedule.java
+++ src/domain/Schedule.java
@@ -11,14 +11,14 @@ public class Schedule extends AbstractEntity{
 	private String title; //予定のタイトル
 	private String description; //予定の詳細
 	private LocalDateTime dateTime; //予定の日時
-	private List<PreparationItem> Items; //予定に紐づく準備物
+	private List<PreparationItem> items; //予定に紐づく準備物
 	
 	
-	public Schedule(String title, LocalDateTime dateTime, List<PreparationItem> Items) {
+	public Schedule(String title, LocalDateTime dateTime, List<PreparationItem> items) {
 		super();
 		this.title = title;
 		this.dateTime = dateTime;
-		this.Items = Items;
+		this.items = items;
 		
 		
 	}
@@ -63,22 +63,22 @@ public class Schedule extends AbstractEntity{
 
 
 	public List<PreparationItem> getItems() {
-		return Items;
+		return items;
 	}
 
 	// 予定を追加するメソッド
-	public void addItems(List<PreparationItem> Items) {
+	public void addItems(List<PreparationItem> items) {
 		// 準備物のリストがnullの場合はスロー
-		if (Items == null) {
+		if (items == null) {
 			throw new IllegalArgumentException("準備物のリストはnullにできません");
 		}
 		
 		// 万が一リストがnullの場合の安全策
-	    if (this.Items == null) {
-	        this.Items = new ArrayList<>();
+	    if (this.items == null) {
+	        this.items = new ArrayList<>();
 	    }
-	    this.Items.add(Items);
-		this.Items = Items;
+	    this.items.addAll(items);
+		this.items = items;
 	}
 	
 	// 予定を削除するメソッド
@@ -86,12 +86,12 @@ public class Schedule extends AbstractEntity{
 		if (item == null) {
 			throw new IllegalArgumentException("削除する準備物はnullにできません");
 		}
-		this.Items.remove(item);
+		this.items.remove(item);
 	}
 	
 	// 表示用の文字に変換するメソッド
 	public String toDisplayString() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		return "予定 [title=" + title + ", description=" + description + ", dateTime=" + dateTime + ", Items=" + Items + "]";
+		return "予定 [title=" + title + ", description=" + description + ", dateTime=" + dateTime + ", Items=" + items + "]";
 	}
 }
