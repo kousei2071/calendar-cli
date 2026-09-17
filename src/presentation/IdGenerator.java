cc8ec0e19d3ce3cc44d2e1e8273dbae4ee5124da
kousei2071 <kousei2071@gmail.com>
2026-09-17 22:36:37 +0900

    feat: idを発行機能追加（idGenerator）

--- src/presentation/IdGenerator.java
+++ src/presentation/IdGenerator.java
@@ -2,5 +2,10 @@ package presentation;
 
 //予定や準備物に重複しない唯一のIDを発行するクラス
 public class IdGenerator {
+	private static int currentId = 0;
 
+	// IDを生成するメソッド
+	public static int generateId() {
+		return ++currentId;
+	}
 }
