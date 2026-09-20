1532d68513e80a83dd53c72d70f544242a702695
kousei2071 <kousei2071@gmail.com>
2026-09-20 23:07:14 +0900

    feat: PreparationItemDtoを作成

--- src/usecase/PreparationItemDto.java
+++ src/usecase/PreparationItemDto.java
@@ -2,5 +2,17 @@ package usecase;
 
 //画面から受け取る準備物データを転送するためのクラス
 public class PreparationItemDto {
+	private String name;
+	
+	public PreparationItemDto(String name) {
+		if (name == null || name.trim().isEmpty()) {
+			throw new IllegalArgumentException("準備物の名前はnullまたは空文字にできません。");
+		}
+		this.name = name;
+	}
 
+	public String getName() {
+		return name;
+	}
+	
 }
